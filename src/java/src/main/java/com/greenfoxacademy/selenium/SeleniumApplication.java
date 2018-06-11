9604e739ad314fdae554c6c858014246071cd717
Pontiac1-1 <suppanmiklos@gmail.com>
2018-06-11 13:49:08 +0200

    remove comments

--- src/java/src/main/java/com/greenfoxacademy/selenium/SeleniumApplication.java
+++ src/java/src/main/java/com/greenfoxacademy/selenium/SeleniumApplication.java
@@ -18,12 +18,6 @@ public class SeleniumApplication {
   public static void main(String[] args) throws Exception {
     SpringApplication.run(SeleniumApplication.class, args);
 
-/**
-    // If you have problem adjusting browser's driver
-    System.setProperty("webdriver.chrome.driver", ".driver/chromedriver");
-*/
-
-    // Start chrome headless mode
     ChromeOptions options = new ChromeOptions();
     List<String> chromeSetup = new ArrayList<>(Arrays.asList(
             "--silent-launch",
@@ -32,39 +26,17 @@ public class SeleniumApplication {
             "headless"));
     options.addArguments(chromeSetup);
 
-    // Instantiate Web Driver
     WebDriver driver = new ChromeDriver(options);
     driver.get("https://google.com");
     System.out.println("Page title: " + driver.getTitle());
 
-    // Search on Google
     driver.findElement(By.name("q")).sendKeys("java junior developer job");
     driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
 
-/**
-    // Another approach with the same result
-    WebElement searchButton = driver.findElement(By
-            .name("btnK"));
-
-    Actions actions = new Actions(driver);
-
-    actions.moveToElement(searchButton).perform();
-
-    WebElement searchField = new WebDriverWait(driver, 10)
-            .until(ExpectedConditions.presenceOfElementLocated(By.name("q")));
-
-    searchField.sendKeys("java junior developer job");
-
-    searchField.submit();
-*/
-
-    // Wait 3 seconds
     Thread.sleep(3000);
 
-    // Print out results
     System.out.println("Total results: " + driver.findElement(By.id("resultStats")).getText());
 
-    // Close the browser
     driver.quit();
   }
 }
